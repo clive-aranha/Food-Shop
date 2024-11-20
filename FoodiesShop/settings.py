9d1647ca75b49ccf80dd470f16860c522159090c
clive-aranha <109789553+clive-aranha@users.noreply.github.com>
2024-11-20 12:16:58 +0530

    Update settings.py

--- FoodiesShop/settings.py
+++ FoodiesShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(g*s90_4ef&t)hsjyc3fawlvjf-&vna&o^7%g)%&r%&=zu=n!t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
